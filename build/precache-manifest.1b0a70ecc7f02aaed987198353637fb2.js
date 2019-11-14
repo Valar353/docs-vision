b02869d8051a793c7b20135279c1ff30bb5ab3be
Valar353 <ivengo24000@mail.ru>
2019-11-14 17:00:04 +0300

    refactoring

---
+++ build/precache-manifest.1b0a70ecc7f02aaed987198353637fb2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4eb8e61e1b6b5adafd7699cf4eba23b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5a15f830a507e735427",
+    "url": "/static/css/main.4946f8b7.chunk.css"
+  },
+  {
+    "revision": "bee321cf97bdb8be429d",
+    "url": "/static/js/2.8a5379d4.chunk.js"
+  },
+  {
+    "revision": "f5a15f830a507e735427",
+    "url": "/static/js/main.529e2b80.chunk.js"
+  },
+  {
+    "revision": "67e6370fa68d8899211c",
+    "url": "/static/js/runtime-main.4fd5f69e.js"
+  }
+]);
